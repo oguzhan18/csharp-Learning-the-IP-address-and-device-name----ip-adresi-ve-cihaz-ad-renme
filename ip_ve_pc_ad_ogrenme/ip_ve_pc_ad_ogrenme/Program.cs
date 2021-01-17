f30bf85ed4548efab2992a6122897fba135df5c5
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2021-01-18 01:32:02 +0300

    csahrp Learning the IP address and device name
    I hope you enjoy the application. If you like it, don't forget to star.

---
+++ ip_ve_pc_ad_ogrenme/ip_ve_pc_ad_ogrenme/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        [Obsolete]
+        private static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed; Console.Clear(); Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Pc ip adressş \n          ve\ncihaz adınız =\n________________________________________________________________");
+            Console.WriteLine("");
+            string hostAdi = Dns.GetHostName();
+            //Makine adını buluyoruz
+
+            Console.WriteLine("Host Adı = " + hostAdi);
+
+            IPHostEntry yerel = Dns.GetHostByName(hostAdi);
+            //yerel değişkeni yaratıp makinede arama sağlayacağız
+
+            foreach (IPAddress ipadres in yerel.AddressList)
+            //döngüye tüm ipleri bulmak için sokuyoruz
+
+            {
+
+                Console.WriteLine("IP Adresi = " + ipadres.ToString());
+                //ipleri yazdırıyoruz
+
+            }
+
+            Console.ReadLine();
+            //console ekranını bekletmek için kullanıyoruz
+
+        }
+    }
+}
